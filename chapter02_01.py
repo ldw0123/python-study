1b18946165e526b12da76b1a5b8b1cdbd3a68ce4
ldw0123 <ldw0123@gmail.com>
2022-01-04 03:49:39 +0900

    Initial commit

---
+++ chapter02_01.py
@@ -0,0 +1,26 @@
+# Chapter02_01
+# 파이썬 완전 기초
+# print 사용법
+
+# 기본 출력
+print('Python Start!')
+print("Python Start!")
+print('''Python start!''')
+print("""Python start!""")
+
+print()     # 줄 바꿈
+
+# separator 옵션
+print('P', 'Y', 'T', 'H', 'O', 'N', sep='')
+print('010', '7777', '1234', sep="-")
+print('python', 'google.com', sep="@")
+
+print()
+
+# end 옵션
+print('Welcome to', end=' ')
+print('IT News', end=' ')
+print('Web Site')
+
+# file 옵션
+import 
---
+++ first.py
@@ -0,0 +1,4 @@
+print('Hello Python Basic!')
+print('Hello Python Basic2')
+print('Hello Python Basic3')
+print('Hello Python Basic4')
